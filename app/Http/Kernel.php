aae858471b0e959cac54425da96adc3e7461c35d
mdjamilaj <mdjamilaj1@gmail.com>
2021-07-20 13:09:38 +0600

    login update

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -3,9 +3,15 @@
 namespace App\Http;
 
 use Illuminate\Foundation\Http\Kernel as HttpKernel;
+use Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful;
 
 class Kernel extends HttpKernel
 {
+
+    // 'api' =&gt; [
+    //     EnsureFrontendRequestsAreStateful::class,
+    //     ...
+    //   ]
     /**
      * The application's global HTTP middleware stack.
      *
